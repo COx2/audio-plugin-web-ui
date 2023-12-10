ff8e470d5922a855dc7791a78ad2cdce3a2c8ef1
Tatsuya Shiozawa <shio.tatsu99@gmail.com>
2023-12-10 21:17:00 +0900

    Add scripts for macOS.

--- Scripts/build_macos_xcode.sh
+++ Scripts/build_macos_xcode.sh
@@ -12,10 +12,10 @@ SCRIPT_DIRECTORY="$(dirname "$(realpath "$0")")"
 cd "${SCRIPT_DIRECTORY}/../"
 
 echo "[audio-plugin-web-ui] Configure from preset..."
-cmake --preset ninja-multi
+cmake --preset xcode
 
 echo "[audio-plugin-web-ui] Build Debug version..."
-cmake --build --preset ninja-debug
+cmake --build --preset xcode-debug
 
 echo "[audio-plugin-web-ui] Build Release version..."
-cmake --build --preset ninja-release
+cmake --build --preset xcode-release
