6bed282d75a531ba1a1155b68079979f6fc81abc
Tatsuya Shiozawa <shio.tatsu99@gmail.com>
2024-04-28 17:31:12 +0900

    Update packages.

--- CMake/Findchoc.cmake
+++ CMake/Findchoc.cmake
@@ -4,4 +4,4 @@ include(CPM)
 
 #Fetching CHOC from git
 #IF you want to instead point it to a local version, you can invoke CMake with
-CPMAddPackage("gh:Tracktion/choc#main")
+CPMAddPackage("gh:Tracktion/choc#c490e9fe5a579623cf17675d1993fb847a9ae522")
--- CMake/Findclap-juce-extensions.cmake
+++ CMake/Findclap-juce-extensions.cmake
@@ -4,4 +4,4 @@ include(CPM)
 
 #Fetching clap-juce-extensions from git
 #IF you want to instead point it to a local version, you can invoke CMake with
-CPMAddPackage("gh:free-audio/clap-juce-extensions#54d83e121545490fdbb2d3dec3ba0c1da6005dde")
+CPMAddPackage("gh:free-audio/clap-juce-extensions#e286d28dc5309b744d748c60723e8d94141e1b3f")
