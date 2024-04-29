14429b6bd8d77906d133bb88f871d54a19f7ff1c
Tatsuya Shiozawa <shio.tatsu99@gmail.com>
2024-04-29 20:07:58 +0900

    Add task tools made with Deno.

---
+++ Tools/deno/build-with-cmake.ts
@@ -0,0 +1,38 @@
+#!/usr/bin/env -S deno run --allow-all
+
+/*
+This script requires the Deno runtime environment to execute.
+Please refer to the official website for instructions on how to install Deno.
+https://deno.com/
+*/
+
+import {$, CommandBuilder} from "https://deno.land/x/dax/mod.ts";
+import { fromFileUrl } from "https://deno.land/std/path/mod.ts";
+
+//============================================
+console.log("Deno.cwd(): " + Deno.cwd());
+
+const __root_directory = fromFileUrl(new URL("../../", import.meta.url));
+Deno.chdir(__root_directory);
+
+console.log("Deno.cwd(): " + Deno.cwd());
+
+const platform = Deno.build.os;
+
+console.log(platform)
+
+if(platform.toString() === "windows")
+{
+    await $`cmake --preset vs2022`;
+    await $`cmake --build --preset vs2022-release`;
+}
+else if(platform.toString() === "darwin")
+{
+    await $`cmake --preset xcode`;
+    await $`cmake --build --preset xcode-release`;
+}
+else if(platform.toString() === "linux")
+{
+    await $`cmake --preset ninja-multi`;
+    await $`cmake --build --preset ninja-release`;
+}
