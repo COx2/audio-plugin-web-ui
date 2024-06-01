c7f1328cebb064eee936d8fed607d8dda63d5ac4
Tatsuya Shiozawa <shio.tatsu99@gmail.com>
2024-06-01 20:50:16 +0900

    Revert "Replace unity test code."
    This reverts commit c4e1f3f3ef03f0175765d8501103d54592db82ce.

--- Plugins/UnitySynth/WebView/vite-project/src/App.tsx
+++ Plugins/UnitySynth/WebView/vite-project/src/App.tsx
@@ -1,26 +1,59 @@
-import { Fragment, useState } from "react";
-import { UnityTest } from "./unity-test";
+import { Fragment, useState, useCallback, useEffect } from 'react'
+import { Unity, useUnityContext } from "react-unity-webgl";
 import './App.css'
 
 function App() {
-  const [isMounted, setIsMounted] = useState(false);
-  const [repaintCounter, setRepaintCounter] = useState(0);
+  const [isGameOver, setIsGameOver] = useState(false);
+  const [userName, setUserName] = useState();
+  const [score, setScore] = useState();
 
-  function handleClickToggleMount() {
-    setIsMounted(!isMounted);
+  const {  unityProvider, isLoaded, loadingProgression, sendMessage, addEventListener, removeEventListener } =
+    useUnityContext({
+      loaderUrl: "unity/builds.loader.js",
+      dataUrl: "unity/builds.data",
+      frameworkUrl: "unity/builds.framework.js",
+      codeUrl: "unity/builds.wasm",
+    });
+
+  const handleGameOver = useCallback((userName: any, score: any) => {
+    setIsGameOver(true);
+    setUserName(userName);
+    setScore(score);
+  }, []);
+
+  useEffect(() => {
+    addEventListener("GameOver", handleGameOver);
+    return () => {
+      removeEventListener("GameOver", handleGameOver);
+    };
+  }, [addEventListener, removeEventListener, handleGameOver]);
+
+  function handleClickCommunicate() {
+    sendMessage("GameController", "Communicate", 100);
   }
+
+  // We'll round the loading progression to a whole number to represent the
+  // percentage of the Unity Application that has loaded.
+  const loadingPercentage = Math.round(loadingProgression * 100);
 
   return (
     <>
       <Fragment>
         <div className="container">
-          <button onClick={handleClickToggleMount}>
-            {isMounted ? "Unmount" : "Mount"} Unity App
-          </button>
-          <button onClick={() => setRepaintCounter((i) => i + 1)}>
-            Force Repaint App ({repaintCounter})
-          </button>
-          {isMounted && <UnityTest />}
+          {isLoaded === false && (
+            // We'll conditionally render the loading overlay if the Unity
+            // Application is not loaded.
+            <div className="loading-overlay">
+              <p>Loading... ({loadingPercentage}%)</p>
+            </div>
+          )}
+          <Unity className="unity" unityProvider={unityProvider} />
+        </div>
+        <div>
+          <button onClick={handleClickCommunicate}>Communicate with Unity</button>
+          {isGameOver === true && (
+            <p>{`Game Over ${userName}! You've scored ${score} points.`}</p>
+          )}
         </div>
       </Fragment>
     </>
